32677041549f52d18be5123c30e1159abe5d3e98
frankyfish <fr.fish93@gmail.com>
2024-12-19 17:39:10 +0100

    migrating to Spring Boot 3 & Spring Framework 6
    * includes switch from javax to jakarta packages (done via migration assistance from Intelij) additionally a bunch of imports has been modified;
    * for Spring* upgrade an attempt was made: specific version have been removed in order to inherit them from the parent project.

--- src/main/java/org/alien4cloud/plugin/kubernetes/modifier/KubernetesCleanUninstallModifier.java
+++ src/main/java/org/alien4cloud/plugin/kubernetes/modifier/KubernetesCleanUninstallModifier.java
@@ -1,54 +1,29 @@
 package org.alien4cloud.plugin.kubernetes.modifier;
 
-import alien4cloud.component.repository.ArtifactRepositoryConstants;
 import alien4cloud.paas.wf.WorkflowSimplifyService;
 import alien4cloud.paas.wf.WorkflowsBuilderService;
 import alien4cloud.paas.wf.util.WorkflowUtils;
 import alien4cloud.paas.wf.validation.WorkflowValidator;
 import alien4cloud.topology.TopologyService;
-import alien4cloud.tosca.context.ToscaContext;
 import alien4cloud.tosca.context.ToscaContextual;
-import alien4cloud.tosca.serializer.ToscaPropertySerializerUtils;
-import alien4cloud.utils.CloneUtil;
-import alien4cloud.utils.MapUtil;
 import alien4cloud.utils.PropertyUtil;
-import alien4cloud.utils.YamlParserUtil;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.alien4cloud.alm.deployment.configuration.flow.FlowExecutionContext;
-import org.alien4cloud.alm.deployment.configuration.flow.TopologyModifierSupport;
 import org.alien4cloud.plugin.kubernetes.AbstractKubernetesModifier;
-import org.alien4cloud.plugin.kubernetes.modifier.helpers.AffinitiyHelper;
-import org.alien4cloud.plugin.kubernetes.modifier.helpers.AntiAffinityHelper;
-import org.alien4cloud.tosca.model.Csar;
 import org.alien4cloud.tosca.model.definitions.*;
 import org.alien4cloud.tosca.model.templates.*;
-import org.alien4cloud.tosca.model.types.CapabilityType;
-import org.alien4cloud.tosca.model.types.NodeType;
-import org.alien4cloud.tosca.model.types.PolicyType;
 import org.alien4cloud.tosca.model.workflow.Workflow;
-import org.alien4cloud.tosca.normative.constants.NormativeCapabilityTypes;
-import org.alien4cloud.tosca.normative.constants.NormativeRelationshipConstants;
 import org.alien4cloud.tosca.normative.constants.NormativeWorkflowNameConstants;
-import org.alien4cloud.tosca.normative.constants.ToscaFunctionConstants;
-import org.alien4cloud.tosca.utils.FunctionEvaluator;
-import org.alien4cloud.tosca.utils.FunctionEvaluatorContext;
 import org.alien4cloud.tosca.utils.TopologyNavigationUtil;
-import org.alien4cloud.tosca.utils.ToscaTypeUtils;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static alien4cloud.utils.AlienUtils.safe;
 import static org.alien4cloud.plugin.kubernetes.modifier.KubeTopologyUtils.*;
-import static org.alien4cloud.plugin.kubernetes.policies.KubePoliciesConstants.*;
 import static org.alien4cloud.tosca.utils.ToscaTypeUtils.isOfType;
 
 /**
